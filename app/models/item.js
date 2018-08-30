4b4573d4fe4d18d67e18b7c1238c11edabdd8e3c
Shyan Kashani <shyankashani1@gmail.com>
2018-08-29 18:32:24 -0700

    Builds out item model

--- app/models/item.js
+++ app/models/item.js
@@ -1,8 +1,80 @@
 import DS from 'ember-data';
 import { computed } from '@ember/object';
+import _ from 'lodash';
 
 export default DS.Model.extend({
-  monkey: computed(function() {
-    return 'goat';
-  })
+
+  /*
+   * Properties
+   */
+  location: DS.attr('string'),
+  notes: DS.attr('string'),
+  staffPick: DS.attr('boolean'),
+
+  /*
+   * Embeded records
+   */
+  game: DS.attr(),
+  color: DS.attr(),
+  category: DS.attr(),
+
+  /*
+   * Game properties
+   */
+  name: computed.reads('game.name'),
+  description: computed.reads('game.description'),
+  yearPublished: computed.reads('game.year_published'),
+  minPlayers: computed.reads('game.min_players'),
+  maxPlayers: computed.reads('game.max_players'),
+  playingTime: computed.reads('game.playing_time'),
+  minPlayTime: computed.reads('game.min_play_time'),
+  maxPlayTime: computed.reads('game.max_play_time'),
+  minAge: computed.reads('game.min_age'),
+  thumbnail: computed.reads('game.thumbnail'),
+  image: computed.reads('game.image'),
+  bggAverageWeight: computed.reads('game.bgg_average_weight'),
+  bggAverageRating: computed.reads('game.bgg_average_rating'),
+
+  /*
+   * Color properties
+   */
+  colorName: computed.reads('color.name'),
+  colorDescription: computed.reads('color.description'),
+  colorHex: computed.reads('color.hex'),
+
+  /*
+   * Category properties
+   */
+  categoryName: computed.reads('category.name'),
+
+  /*
+   * Computed properties
+   */
+  longDescription: computed('description', function() {
+    const description = this.get('description');
+    return description.split('&#10;').map(p => p.length ? _.unescape(p) : '');
+  }),
+
+  shortDescription: computed('longDescription', function() {
+    const longDescription = this.get('longDescription');
+    return _.truncate(longDescription[0], { length: 480, separator: '.', omission: '.' })
+  }),
+
+  numberOfPlayers: computed('minPlayers', 'maxPlayers', function() {
+    const minPlayers = this.get('minPlayers');
+    const maxPlayers = this.get('maxPlayers');
+
+    return minPlayers < maxPlayers
+      ? `${minPlayers}-${maxPlayers} players`
+      : `${maxPlayers} players`;
+  }),
+
+  playTime: computed('minPlayTime', 'maxPlayTime', function() {
+    const minPlayTime = this.get('minPlayTime');
+    const maxPlayTime = this.get('maxPlayTime');
+
+    return minPlayTime < maxPlayTime
+      ? `${minPlayTime}-${maxPlayTime} minutes`
+      : `${maxPlayTime} minutes`;
+  }),
 });
