57c8196c2b73163d37948181fdb8115a2c4299f2
Shyan Kashani <shyankashani1@gmail.com>
2018-09-03 20:29:55 -0700

    Updates models

--- app/models/game.js
+++ app/models/game.js
@@ -1,19 +1,54 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
+import _ from 'lodash';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
   yearPublished: DS.attr('number'),
-  minPlayers: DS.attr('number'),
-  maxPlayers: DS.attr('number'),
-  playingTime: DS.attr('number'),
-  minPlayTime: DS.attr('number'),
-  maxPlayTime: DS.attr('number'),
-  minAge: DS.attr('number'),
+  min_players: DS.attr('number'),
+  max_players: DS.attr('number'),
+  playing_time: DS.attr('number'),
+  min_play_time: DS.attr('number'),
+  max_play_time: DS.attr('number'),
+  min_age: DS.attr('number'),
   thumbnail: DS.attr('string'),
   image: DS.attr('string'),
-  bggAverageWeight: DS.attr('number'),
-  bggAverageRating: DS.attr('number'),
+  bgg_average_weight: DS.attr('number'),
+  bgg_average_rating: DS.attr('number'),
 
-  items: DS.hasMany('items')
+  items: DS.hasMany('items'),
+
+  longDescription: computed('description', function() {
+    const description = this.get('description');
+    return description.split('&#10;').map(p => p.length ? _.unescape(p) : '');
+  }),
+
+  shortDescription: computed('longDescription', function() {
+    const longDescription = this.get('longDescription');
+    return _.truncate(longDescription[0], { length: 480, separator: '.', omission: '.' })
+  }),
+
+  numberOfPlayers: computed('min_players', 'max_players', function() {
+    const min_players = this.get('min_players');
+    const max_players = this.get('max_players');
+
+    return min_players < max_players
+      ? `${min_players}-${max_players} players`
+      : `${max_players} players`;
+  }),
+
+  playTime: computed('min_play_time', 'max_play_time', function() {
+    const min_play_time = this.get('min_play_time');
+    const max_play_time = this.get('max_play_time');
+
+    return min_play_time < max_play_time
+      ? `${min_play_time}-${max_play_time} minutes`
+      : `${max_play_time} minutes`;
+  }),
+
+  minimumAge: computed('min_age', function() {
+    const min_age = this.get('min_age');
+    return `${min_age} years and up`;
+  }),
 });
--- app/models/item.js
+++ app/models/item.js
@@ -1,99 +1,12 @@
 import DS from 'ember-data';
-import { computed } from '@ember/object';
-import _ from 'lodash';
-
-const BADGE_COLORS = {
-  1: 'success',
-  2: 'warning',
-  3: 'danger'
-};
 
 export default DS.Model.extend({
-
-  /*
-   * Properties
-   */
   location: DS.attr('string'),
   notes: DS.attr('string'),
   staffPick: DS.attr('boolean'),
 
-  /*
-   * Embeded records
-   */
   organization: DS.belongsTo('organization'),
   game: DS.belongsTo('game'),
   difficulty: DS.belongsTo('difficulty'),
-  category: DS.belongsTo('category'),
-
-  /*
-   * Game properties
-   */
-  name: computed.reads('game.name'),
-  description: computed.reads('game.description'),
-  yearPublished: computed.reads('game.year_published'),
-  minPlayers: computed.reads('game.min_players'),
-  maxPlayers: computed.reads('game.max_players'),
-  playingTime: computed.reads('game.playing_time'),
-  minPlayTime: computed.reads('game.min_play_time'),
-  maxPlayTime: computed.reads('game.max_play_time'),
-  minAge: computed.reads('game.min_age'),
-  thumbnail: computed.reads('game.thumbnail'),
-  image: computed.reads('game.image'),
-  bggAverageWeight: computed.reads('game.bgg_average_weight'),
-  bggAverageRating: computed.reads('game.bgg_average_rating'),
-
-  /*
-   * Difficulty properties
-   */
-  difficultyId: computed.reads('difficulty.id'),
-  difficultyName: computed.reads('difficulty.name'),
-  difficultyDescription: computed.reads('difficulty.description'),
-  difficultyHex: computed.reads('difficulty.hex'),
-
-  /*
-   * Category properties
-   */
-  categoryId: computed.reads('category.id'),
-  categoryName: computed.reads('category.name'),
-
-  /*
-   * Computed properties
-   */
-  longDescription: computed('description', function() {
-    const description = this.get('description');
-    return description.split('&#10;').map(p => p.length ? _.unescape(p) : '');
-  }),
-
-  shortDescription: computed('longDescription', function() {
-    const longDescription = this.get('longDescription');
-    return _.truncate(longDescription[0], { length: 480, separator: '.', omission: '.' })
-  }),
-
-  numberOfPlayers: computed('minPlayers', 'maxPlayers', function() {
-    const minPlayers = this.get('minPlayers');
-    const maxPlayers = this.get('maxPlayers');
-
-    return minPlayers < maxPlayers
-      ? `${minPlayers}-${maxPlayers} players`
-      : `${maxPlayers} players`;
-  }),
-
-  playTime: computed('minPlayTime', 'maxPlayTime', function() {
-    const minPlayTime = this.get('minPlayTime');
-    const maxPlayTime = this.get('maxPlayTime');
-
-    return minPlayTime < maxPlayTime
-      ? `${minPlayTime}-${maxPlayTime} minutes`
-      : `${maxPlayTime} minutes`;
-  }),
-
-  minimumAge: computed('minAge', function() {
-    const minAge = this.get('minAge');
-    return `${minAge} years and up`;
-  }),
-
-  badgeColor: computed('difficultyId', function() {
-    const difficultyId = this.get('difficultyId');
-    return BADGE_COLORS[difficultyId];
-  })
+  category: DS.belongsTo('category')
 });
