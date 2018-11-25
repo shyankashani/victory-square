719e53d131e74731568fd2c072e1a5b32fe16dd5
Shyan Kashani <shyankashani1@gmail.com>
2018-11-25 00:41:03 -0800

    Builds organizations organization inventory index route

--- app/routes/organizations/organization/inventory/index.js
+++ app/routes/organizations/organization/inventory/index.js
@@ -1,4 +1,9 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model(params, transition) {
+    return this.store.query('item', {
+      'filter{organization}': transition.params['organizations.organization'].organization_id
+    });
+  }
 });
